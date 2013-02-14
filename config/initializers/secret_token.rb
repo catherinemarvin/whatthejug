3383d2a8780f41e0fb6e4148bfe4b0b949f0b57b
Kevin Hwang <k3vinhwang@gmail.com>
2013-02-13 22:20:22 -0800

    Rails initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whatthejug::Application.config.secret_token = 'a32519baae510bfe707ffddaa493463f16bb1693aa052fedadbf933ef008dee3eac6e8306f1b2027b6959428fce9137f7a92d35d6fd7dbef38419b0c964f55f1'
