7b9ce411248066d958273e01afc2d8f3bdd9227f
Kevin Hwang <k3vinhwang@gmail.com>
2013-03-16 17:19:28 -0700

    Change path for local images and ignore both tag file and this image repository

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,9 @@ class Image < ActiveRecord::Base
   attr_accessible :unreviewed, :url, :tags
 
   attr_accessible :data
-  has_attached_file :data
+  has_attached_file :data,
+    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+    :url => "/system/:attachment/:id/:style/:filename"
 
   acts_as_taggable
 end
