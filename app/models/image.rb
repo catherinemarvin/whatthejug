6405eeed1435bff0d8f1a0a7a88c093ebb3b99bf
Kevin Hwang <k3vinhwang@gmail.com>
2013-03-18 23:13:21 -0700

    Remove "unreviewed" option when submitting a picture and set it to true on default

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,9 +2,16 @@ class Image < ActiveRecord::Base
   attr_accessible :unreviewed, :url, :tags
 
   attr_accessible :data
+
+  before_create :set_default_values
+
   has_attached_file :data,
     :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
     :url => "/system/:attachment/:id/:style/:filename"
 
   acts_as_taggable
+
+  def set_default_values
+    self.unreviewed ||= true
+  end
 end
